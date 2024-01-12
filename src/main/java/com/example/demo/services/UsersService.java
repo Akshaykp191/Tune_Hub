edb2eaddaa4f0cab75f129466c03492b90da5988
Akshay <Akshay@Akshay>
2024-01-12 15:37:19 +0530

    1st Version of tunehub

---
+++ src/main/java/com/example/demo/services/UsersService.java
@@ -0,0 +1,13 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.Users;
+
+public interface UsersService {
+	public String adddUsers(Users user);
+	public boolean emailExists(String email);
+	public boolean validateUser(String email, String password);
+	public String getRole(String email);
+	
+	public Users getuser(String email);
+	public void updateUser(Users user);
+}
